aa813797fddefc1a97ae5b0a2b0d89997af5d629
Lucas <uWA%VblDP1>
2022-02-22 18:06:09 -0300

    lucas

--- src/components/AddItem.jsx
+++ src/components/AddItem.jsx
@@ -1,203 +1,203 @@
-import { Button, Container, FormControl, FormControlLabel, FormLabel, Grid, makeStyles, Radio, RadioGroup, TextField } from '@material-ui/core';
-import { Typography } from '@mui/material';
-import React from 'react';
-import { useState } from 'react';
-import AddIcon from '@mui/icons-material/Add';
-import CloudUploadIcon from '@mui/icons-material/CloudUpload';
-
+import {
+  Button,
+  Container,
+  FormControl,
+  FormControlLabel,
+  FormLabel,
+  Grid,
+  makeStyles,
+  Radio,
+  RadioGroup,
+  TextField,
+} from "@material-ui/core";
+import { Typography } from "@mui/material";
+import React from "react";
+import { useState } from "react";
+import AddIcon from "@mui/icons-material/Add";
+import CloudUploadIcon from "@mui/icons-material/CloudUpload";
 
 const useStyles = makeStyles({
-    field: {
-        marginTop: 20,
-        marginBottom: 20,
-        display: 'block'
-    },
-    row: {
-        display: 'flex',
-        padding: 10,
-        margin: 10
-    },
-    item: {
-        margin: 5,
-        padding: 5,
-        justifyContent: "right",
-    },
-})
-
+  field: {
+    marginTop: 20,
+    marginBottom: 20,
+    display: "block",
+  },
+  row: {
+    display: "flex",
+    padding: 10,
+    margin: 10,
+  },
+  item: {
+    margin: 5,
+    padding: 5,
+    justifyContent: "right",
+  },
+});
 
 function AddItem(props) {
-    const classes = useStyles()
-    const [nameSet, setNameSet] = useState('')
-    const [numberSet, setNumberSet] = useState('')
-    const [description, setDescription] = useState('')
-    const [complete, setComplete] = useState("complete")
-    const [value, setValue] = useState(0.0)
-    const [selectedFile, setSelectedFile] = useState();
-    const [isFilePicked, setIsFilePicked] = useState(false);
-    const [error, setError] = useState(false)
+  const classes = useStyles();
+  const [nameSet, setNameSet] = useState("");
+  const [numberSet, setNumberSet] = useState("");
+  const [description, setDescription] = useState("");
+  const [complete, setComplete] = useState("complete");
+  const [value, setValue] = useState(0.0);
+  const [selectedFile, setSelectedFile] = useState("");
+  const [isFilePicked, setIsFilePicked] = useState(false);
+  const [error, setError] = useState(false);
 
-    const changeHandler = (e) => {
-        setSelectedFile(e.target.files[0]);
-        setIsFilePicked(true);
-    };
-    const hiddenFileInput = React.useRef(null);
-  
-    // Programatically click the hidden file input element
-    // when the Button component is clicked
-    const handleClick = event => {
-      hiddenFileInput.current.click();
-    };
-    // Call a function (passed as a prop from the parent component)
-    // to handle the user-selected file 
-    const handleChange = event => {
-      const fileUploaded = event.target.files[0];
-      props.handleFile(fileUploaded);
-    };
-    const handleSubmit = (e) => {
-        e.preventDefault()
-        setError(false)
-        if (numberSet == '') {
-            setError(true)
-        }
-        const jsonProduct = {
-            "numberSet": numberSet,
-            "nameSet": nameSet,
-            "complete": complete,
-            "description": description,
-            "value": value
-        }
+  const changeHandler = (e) => {
+    setSelectedFile(e.target.files[0]);
+    setIsFilePicked(true);
+  };
+  const hiddenFileInput = React.useRef(null);
+
+  // Programatically click the hidden file input element
+  // when the Button component is clicked
+  const handleClick = (event) => {
+    hiddenFileInput.current.click();
+  };
 
 
-        console.log(JSON.stringify(jsonProduct));
-
-        //   if (title && details) {
-        //     fetch('http://localhost:8000/notes', {
-        //       method: 'POST',
-        //       headers: {"Content-type": "application/json"},
-        //       body: JSON.stringify({ title, details, category })
-        //     }).then(() => redirect'/'))
-        //   } 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setError(false);
+    if (numberSet == "") {
+      setError(true);
     }
+    const formData = new FormData();
 
-    return (
-        <Container size="sm">
+    formData.append('File', selectedFile);
+    const jsonProduct = {
+      numberSet: numberSet,
+      nameSet: nameSet,
+      complete: complete,
+      description: description,
+      value: value,
+    };
 
+    console.log(JSON.stringify(jsonProduct));
 
-            <form noValidate autoComplete="off" onSubmit={handleSubmit} >
-                <Grid
-                    container
-                    spacing={2}
-                    direction="row"
-                    alignItems="left"
-                    justifyContent="left"
+    //   if (title && details) {
+    //     fetch('http://localhost:8000/notes', {
+    //       method: 'POST',
+    //       headers: {"Content-type": "application/json"},
+    //       body: JSON.stringify({ title, details, category })
+    //     }).then(() => redirect'/'))
+    //   }
+  };
 
-                >
-                    <Grid item md={12} lg={12} xs={12}>
-
-                        <Typography
-                            variant="h4"
-                            color="textSecondary"
-                            component="h2"
-
-                        >
-                            Cadastrar Lego
-                        </Typography>
-                    </Grid>
-                    <Grid item md={8} lg={8} xs={8}>
-
-                        <TextField
-                            onChange={(e) => setNameSet(e.target.value)}
-                            label="Nome Set"
-                            variant="outlined"
-                            color="secondary"
-                            fullWidth
-                            required
-
-                        />
-
-                    </Grid>
-                    <Grid item md={4} lg={4}>
-
-                        <TextField
-                            onChange={(e) => setNumberSet(e.target.value)}
-                            label="Número Set"
-                            variant="outlined"
-                            color="secondary"
-                            fullWidth
-                            required
-
-                        />
-
-                    </Grid>
-                    <Grid item md={12} lg={12}>
-                        <TextField
-                            onChange={(e) => setDescription(e.target.value)}
-                            label="Descrição"
-                            variant="outlined"
-                            color="secondary"
-                            multiline
-                            rows={3}
-                            fullWidth
-                            required
-                        />
-                    </Grid>
-                    <Grid item md={12} lg={12}>
-                        <TextField
-                            onChange={(e) => setValue(parseFloat(e.target.value))}
-                            label="Valor"
-                            variant="outlined"
-                            color="secondary"
-                            fullWidth
-                            required
-                        />
-                    </Grid>
-                    <Grid item md={2} lg={2} >
-                        <Typography variant="h6" color="#000">Foto do Set(.jpg)</Typography></Grid>
-                    <Grid item md={3} lg={3} >
-
-                       <Button color="secondary"
+  return (
+    <Container size="sm">
+      <form noValidate autoComplete="off" onSubmit={handleSubmit}>
+        <Grid
+          container
+          spacing={2}
+          direction="row"
+          alignItems="left"
+          justifyContent="left"
+        >
+          <Grid item md={12} lg={12} xs={12}>
+            <Typography variant="h4" color="textSecondary" component="h2">
+              Cadastrar Lego
+            </Typography>
+          </Grid>
+          <Grid item md={8} lg={8} xs={8}>
+            <TextField
+              onChange={(e) => setNameSet(e.target.value)}
+              label="Nome Set"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item md={4} lg={4}>
+            <TextField
+              onChange={(e) => setNumberSet(e.target.value)}
+              label="Número Set"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item md={12} lg={12}>
+            <TextField
+              onChange={(e) => setDescription(e.target.value)}
+              label="Descrição"
+              variant="outlined"
+              color="secondary"
+              multiline
+              rows={3}
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item md={12} lg={12}>
+            <TextField
+              onChange={(e) => setValue(parseFloat(e.target.value))}
+              label="Valor"
+              variant="outlined"
+              color="secondary"
+              fullWidth
+              required
+            />
+          </Grid>
+          <Grid item md={2} lg={2}>
+            <Typography variant="h6" color="#000">
+              Foto do Set(.jpg)
+            </Typography>
+          </Grid>
+          <Grid item md={3} lg={3}>
+            {/* <Button color="secondary"
                             variant="contained"  type="file" name="file" endIcon={<CloudUploadIcon />} 
-                            onClick={handleClick}> Upload</Button>
-      <input
-        type="file"
-        ref={hiddenFileInput}
-        onChange={handleChange}
-        style={{display: 'none'}} {/* Make the file input element invisible */}
-      />
-                   
+                            onClick={handleClick}> Upload</Button> */}
 
-                    </Grid>
-                    <Grid item md={5} lg={5}>
-                        <FormControl className={classes.field}>
-                            <FormLabel>Estado</FormLabel>
-                            <RadioGroup value={complete} onChange={(e) => setComplete(e.target.value)}>
-                                <FormControlLabel value="complete" control={<Radio />} label="Completo" />
-                                <FormControlLabel value="incomplete" control={<Radio />} label="Incompleto" />
-                            </RadioGroup>
-                        </FormControl>
-                    </Grid>
-                    <Grid container direction="row"
-                        justifyContent="center"
-                        alignItems="center">
-                        <Grid item>
-                            <Button
-                                type="submit"
-                                color="primary"
-                                variant="contained"
-                                endIcon={<AddIcon />}
-                            >
-
-                                Cadastrar
-                            </Button >
-                        </Grid>
-                    </Grid>
-                </Grid>
-            </form>
-
-
-        </Container>
-
-    );
+            <div>
+              <input type="file" name="file" onChange={changeHandler} />
+            </div>
+          </Grid>
+          <Grid item md={5} lg={5}>
+            <FormControl className={classes.field}>
+              <FormLabel>Estado</FormLabel>
+              <RadioGroup
+                value={complete}
+                onChange={(e) => setComplete(e.target.value)}
+              >
+                <FormControlLabel
+                  value="complete"
+                  control={<Radio />}
+                  label="Completo"
+                />
+                <FormControlLabel
+                  value="incomplete"
+                  control={<Radio />}
+                  label="Incompleto"
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid
+            container
+            direction="row"
+            justifyContent="center"
+            alignItems="center"
+          >
+            <Grid item>
+              <Button
+                type="submit"
+                color="primary"
+                variant="contained"
+                endIcon={<AddIcon />}
+              >
+                Cadastrar
+              </Button>
+            </Grid>
+          </Grid>
+        </Grid>
+      </form>
+    </Container>
+  );
 }
 
 export default AddItem;
